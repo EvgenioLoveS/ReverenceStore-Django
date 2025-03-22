0723dcf7050c0566793efbfb8b4829458555d70e
EvgenioLoveS <zhgutov.zhenyaq@gmail.com>
2025-03-22 21:54:50 +0300

    models and catalog views have been made

---
+++ reverence/main/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from .models import Size, Category, \
+    ClothingItem, ClothingItemSize, ItemImage
+# Register your models here.
+
+
+
+class ClothingItemSizeInline(admin.TabularInline):
+    model = ClothingItemSize
+    extra = 4
+
+
+class ItemImageInline(admin.TabularInline):
+    model = ItemImage
+    extra = 4
+
+
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+
+
+@admin.register(ClothingItem)
+class ClothingItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'category',
+                    'available', 'price', 'discount',
+                    'created_at', 'updated_at')
+    list_filter = ('available', 'category')
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ('-created_at',)
+    inlines = [ClothingItemSizeInline, ItemImageInline]
